65ecc31ff28f6777ac0336c303ff9b332b8d987d
Yuliia-Yul <yulianna.ego@gmail.com>
2023-04-22 20:51:21 +0300

    finished task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,65 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+
+const itemsMarkup = createGalleryMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", itemsMarkup);
+galleryContainer.addEventListener("click", onImageClick);
+
+const instance = basicLightbox.create(
+  `
+    <img src=" " width = "1280" height = "auto" >`,
+  {
+    onShow: (instance) => {
+      window.addEventListener("keydown", closeOnEsc);
+    },
+    onClose: (instance) => {
+      window.removeEventListener("keydown", closeOnEsc);
+    },
+  }
+);
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </li>`;
+    })
+    .join("");
+}
+
+function onImageClick(event) {
+  event.preventDefault();
+  const datasetSource = event.target.dataset.source;
+  if (!datasetSource) return;
+  instance.element().querySelector("img").src = datasetSource;
+  instance.show();
+}
+
+function closeOnEsc(event) {
+  if (event.code === "Escape") {
+    instance.close();
+  }
+}
+
+function onImgClick(e) {
+  e.preventDefault();
+  const datasetSource = e.target.dataset.source;
+  if (!datasetSource) return;
+  instance.element().querySelector("img").src = datasetSource;
+  instance.show();
+}
+
+function onEscKeyPress(e) {
+  if (e.code !== "Escape") return;
+  instance.close();
+}
